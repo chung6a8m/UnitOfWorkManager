3f7c1f0db3b1ed5471f3f420e17b4102ae3adcae
chungpd <chung6a8m@yahoo.com>
2026-08-07 03:15:29 +0700

    test: force Testing environment for Web API sample

--- tests/UnitOfWork.Sample.WebApi.Tests/WebApiSampleTests.cs
+++ tests/UnitOfWork.Sample.WebApi.Tests/WebApiSampleTests.cs
@@ -1,14 +1,15 @@
 using System.Net.Http.Json;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Xunit;
 
 namespace UnitOfWork.Sample.WebApi.Tests;
 
-public sealed class WebApiSampleTests : IClassFixture<WebApplicationFactory<Program>>
+public sealed class WebApiSampleTests : IClassFixture<WebApiApplicationFactory>
 {
     private readonly HttpClient _client;
 
-    public WebApiSampleTests(WebApplicationFactory<Program> factory)
+    public WebApiSampleTests(WebApiApplicationFactory factory)
     {
         _client = factory.CreateClient();
     }
@@ -71,4 +72,12 @@ public sealed class WebApiSampleTests : IClassFixture<WebApplicationFactory<Prog
         bool AmbientClearedAfterCancellation,
         bool ConcurrencyGuardObserved,
         bool AllPassed);
+}
+
+public sealed class WebApiApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseEnvironment("Testing");
+    }
 }
